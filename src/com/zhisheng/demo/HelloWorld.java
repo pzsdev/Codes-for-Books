9d39fdacd7116d1e2d3037ab19a6e51762acd0f2
pengzhisheng <pzs034@gmail.com>
2019-10-18 02:35:08 +0800

    hello IDEA !

--- src/com/zhisheng/demo/HelloWorld.java
+++ src/com/zhisheng/demo/HelloWorld.java
@@ -3,5 +3,6 @@ package com.zhisheng.demo;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("This is from IDEA, and commit from IDEA.");
     }
 }
