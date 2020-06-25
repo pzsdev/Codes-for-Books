f0176cd2d8177fee4000f2c27cd5e37f020fe268
pengzhisheng <pzs034@gmail.com>
2020-06-26 01:30:19 +0800

    add chapter 22 demo, Observer pattern

---
+++ src/com/zhisheng/designpatterns/chapter22observerpattern/LiuSi.java
@@ -0,0 +1,17 @@
+package com.zhisheng.designpatterns.chapter22observerpattern;
+
+/**
+ * 具体观察者，刘斯
+ */
+public class LiuSi implements Observer {
+    @Override
+    public void update(String string) {
+        System.out.println("刘斯：观察到韩非子活动，自己也开始活动了。。。");
+        this.happy(string);
+        System.out.println("刘斯：乐死了。。。\n");
+    }
+
+    private void happy(String reportContext) {
+        System.out.println("刘斯：因为" + reportContext + "，所以我开心啊！");
+    }
+}
