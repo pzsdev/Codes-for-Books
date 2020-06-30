769500eb3d1e24698ab217f13e065e74475d2d51
pengzhisheng <pzs034@gmail.com>
2020-06-30 23:49:09 +0800

    add chapter 10 demo, template mothod pattern

---
+++ src/com/zhisheng/designpatterns/chapter10templatemethodpattern/HummerH1Model.java
@@ -0,0 +1,26 @@
+package com.zhisheng.designpatterns.chapter10templatemethodpattern;
+
+/**
+ * H1 型号悍马模型
+ */
+public class HummerH1Model extends HummerModel {
+    public void start() {
+        System.out.println("悍马H1 发动");
+    }
+
+    public void enginBoom() {
+        System.out.println("悍马H1 引擎声音是这样的。。。");
+    }
+
+    public void alarm() {
+        System.out.println("悍马H1 鸣笛");
+    }
+
+    public void stop() {
+        System.out.println("悍马H1 停车。。。");
+    }
+
+    public void run() {
+        super.run();
+    }
+}
