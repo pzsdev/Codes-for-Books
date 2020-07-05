09999eb5f21e61c767ee43faaa42842ff4ee6986
pengzhisheng <pzs034@gmail.com>
2020-07-05 11:35:37 +0800

    add chapter 11 demo, builder pattern

---
+++ src/com/zhisheng/designpatterns/chapter11builderpattern/CarBuilder.java
@@ -0,0 +1,13 @@
+package com.zhisheng.designpatterns.chapter11builderpattern;
+
+import java.util.ArrayList;
+
+/**
+ * 抽象汽车组装者
+ */
+public abstract class CarBuilder {
+    // 建造一个模型，设置具体的操作执行顺序
+    public abstract void setSequence(ArrayList<String> sequence);
+    // 设置完毕，返回一个这个汽车模型
+    public abstract CarModel getCarModel();
+}
