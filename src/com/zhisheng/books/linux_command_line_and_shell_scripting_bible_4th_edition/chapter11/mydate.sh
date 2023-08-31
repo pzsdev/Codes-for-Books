941bf4b5af2776f7d18be3abaf890602a1ddd4e5
pengzhisheng <pzs034@gmail.com>
2023-08-31 17:37:05 +0800

    update

---
+++ src/com/zhisheng/books/linux_command_line_and_shell_scripting_bible_4th_edition/chapter11/mydate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# calculate the number of days between two dates
+date1="Jan 1, 2020"
+date2="May 1, 2020"
+
+time1=$(date -d "$date1" +%s)
+time2=$(date -d "$date2" +%s)
+
+diff=$(expr $time2 - $time1)
+secondsinday=$(expr 60 \* 60 \* 24)
+days=$(expr $diff / $secondsinday)
+
+echo "The difference between $date1 and $date2 is $days days."
--- src/com/zhisheng/books/the_logic_of_java_programming/chapter7/ChapterSeven.java
+++ src/com/zhisheng/books/the_logic_of_java_programming/chapter7/ChapterSeven.java
@@ -1,6 +1,8 @@
 package com.zhisheng.books.the_logic_of_java_programming.chapter7;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 /**
@@ -14,8 +16,10 @@ public class ChapterSeven {
         System.out.println("Chapter Seven");
 
         ChapterSeven chapterSeven = new ChapterSeven();
-        chapterSeven.date();
-        chapterSeven.timeZone();
+//        chapterSeven.date();
+//        chapterSeven.timeZone();
+//        chapterSeven.locale();
+        chapterSeven.calender();
     }
 
     /**
@@ -68,6 +72,86 @@ public class ChapterSeven {
     public void timeZone() {
         /* TimeZone */
         TimeZone timeZone = TimeZone.getDefault();//获取当前时区
-        System.out.println(timeZone);
+        System.out.println(timeZone.getID());
+
+        String property = System.getProperty("user.timezone");
+        System.out.println(property);
+
+        // 获取任意时区实例
+        TimeZone timeZone1 = TimeZone.getTimeZone("GMT+8");
+        System.out.println(timeZone1.getID());
+        TimeZone timeZone2 = TimeZone.getTimeZone("US/Central");
+        System.out.println(timeZone2.getID());
+    }
+
+    public void locale() {
+        /* Locale */
+        // 表示国家（或地区）和语言，两个参数，国家（或地区）和语言
+        // 有一些默认的静态变量，如 Locale.CHINA、Locale.US、Locale.ENGLISH
+
+        Locale aDefault = Locale.getDefault();
+        System.out.println(aDefault.toString());
+
+        Locale locale = Locale.SIMPLIFIED_CHINESE;
+        System.out.println(locale.toString());
+        Locale us = Locale.US;
+        System.out.println(us.toString());
+
+    }
+
+    public void calender() {
+        /* Calendar */
+        // Calendar 是一个抽象类，不能直接实例化，可以通过 getInstance() 方法获取一个 Calendar 对象
+        // 是日期和时间操作的主要类，提供了很多方法，如获取年、月、日、时、分、秒等
+        // 表示与 TimeZone 和 Locale 相关的日历，可以将其转换为 Date 对象
+
+        // 内部有一个字段，表示当前日历所表示的时间，是一个 long 类型的整数，表示自 1970 年 1 月 1 日 00:00:00 GMT 以来的毫秒数
+        // 通过 getTimeInMillis() 方法获取这个字段的值
+        // 还有一个数组，存储了当前日历所表示的时间的年、月、日、时、分、秒等信息，长度为 17，通过 get() 方法获取这个数组的值
+
+        // Calendar 常用方法
+        Calendar calendar = Calendar.getInstance();
+        println("year ", calendar.get(Calendar.YEAR));
+        println("month ", calendar.get(Calendar.MONTH));
+        println("date ", calendar.get(Calendar.DATE));
+        println("hour ", calendar.get(Calendar.HOUR));
+        println("minute ", calendar.get(Calendar.MINUTE));
+        println("second ", calendar.get(Calendar.SECOND));
+        println("day of week ", calendar.get(Calendar.DAY_OF_WEEK));
+        println("day of year ", calendar.get(Calendar.DAY_OF_YEAR));
+        println("day of month ", calendar.get(Calendar.DAY_OF_MONTH));
+        println("day of week in month ", calendar.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+
+        // 加减
+        calendar.add(Calendar.DATE, 100);
+        println("add 100 days ", calendar.get(Calendar.DATE));
+        calendar.add(Calendar.DAY_OF_MONTH, -100);
+        println("add -100 days ", calendar.get(Calendar.DATE));
+
+        Calendar instance = Calendar.getInstance(TimeZone.getTimeZone("GMT+0"), Locale.US);
+        println("year ", instance.get(Calendar.YEAR));
+        println("month ", instance.get(Calendar.MONTH));
+        println("date ", instance.get(Calendar.DATE));
+        println("hour ", instance.get(Calendar.HOUR));
+        println("minute ", instance.get(Calendar.MINUTE));
+        println("second ", instance.get(Calendar.SECOND));
+        println("day of week ", instance.get(Calendar.DAY_OF_WEEK));
+        println("day of year ", instance.get(Calendar.DAY_OF_YEAR));
+        println("day of month ", instance.get(Calendar.DAY_OF_MONTH));
+        println("day of week in month ", instance.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+    }
+
+    public void format() {
+
+    }
+
+
+    public void println(Object ...args) {
+        // 连接成一个字符串打印出来
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Object arg : args) {
+            stringBuilder.append(arg);
+        }
+        System.out.println(stringBuilder);
     }
 }
